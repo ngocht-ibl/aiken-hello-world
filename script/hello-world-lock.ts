72db11a94b406ee26a181c94b672a2440a3dd1cb
ngocht2 <ngocht@blockchainlabs.asia>
2024-03-22 14:31:51 +0700

    update the code for using the contract

---
+++ script/hello-world-lock.ts
@@ -0,0 +1,35 @@
+import {
+  Blockfrost,
+  C,
+  Constr,
+  Data,
+  Lucid,
+  SpendingValidator,
+  TxHash,
+  fromHex,
+  toHex,
+  utf8ToHex,
+} from "https://deno.land/x/lucid@0.8.3/mod.ts";
+import * as cbor from "https://deno.land/x/cbor@v1.4.1/index.js";
+ 
+const lucid = await Lucid.new(
+  new Blockfrost(
+    "https://cardano-preview.blockfrost.io/api/v0",
+    Deno.env.get("BLOCKFROST_PROJECT_ID")
+  ),
+  "Preview"
+);
+
+lucid.selectWalletFromPrivateKey(await Deno.readTextFile("./me.sk"));
+
+const validator = await readValidator();
+
+// --- Supporting functions
+
+async function readValidator(): Promise<SpendingValidator> {
+  const validator = JSON.parse(await Deno.readTextFile("plutus.json")).validators[0];
+  return {
+    type: "PlutusV2",
+    script: toHex(cbor.encode(fromHex(validator.compiledCode))),
+  };
+}
